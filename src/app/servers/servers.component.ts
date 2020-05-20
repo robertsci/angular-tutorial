293ae8c054ffeed1ad5cad54a62dfae6802fc841
Robert Constantinescu <robertsci@github.com>
2020-05-20 08:24:50 +0300

    using *ngIf in the html file

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
   serverName: string = 'TestServer';
+  serverCreated: boolean = false;
 
   constructor() {
     setTimeout(()=> {
@@ -20,7 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(): void {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is: ' +this.serverName;
   }
   onUpdateServerName(event: Event) {
     console.log(event);
