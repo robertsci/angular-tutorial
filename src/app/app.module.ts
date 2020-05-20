18ec304c56bd163da0d53642ed9460545f7af528
Robert Constantinescu <robertsci@github.com>
2020-05-20 19:47:11 +0300

    finished assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from "./success-alert/success-alert.component";
 import {FormsModule} from "@angular/forms";
+import { AppBindingComponent } from './app-binding/app-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppBindingComponent,
   ],
     imports: [
         BrowserModule,
