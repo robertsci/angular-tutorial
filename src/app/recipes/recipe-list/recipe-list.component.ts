75cfc929a886403766445bf6c37a55101ae8c68c
Robert Constantinescu <robertsci@github.com>
2020-05-21 08:12:10 +0300

    some working prototype of the app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+
+  recipes: Recipe[] = [
+    new Recipe("CheeseCake",
+            "cookie with chees",
+            "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646"),
+    new Recipe("CheeseCake",
+      "cookie with chees",
+      "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646")
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
