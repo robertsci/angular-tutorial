2cc6f714b8a0ddee939e7b44a314e317dcac8cd9
Robert Constantinescu <robertsci@github.com>
2020-05-21 18:54:18 +0300

    revert previous naming

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   @Output()
   serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  @Output('bpCreated')
+  @Output()
   blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   newServerName = '';
