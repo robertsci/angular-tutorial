da65c37293ed242f9a1090e33d9a6c60d8c3a22d
Robert Constantinescu <robertsci@github.com>
2020-05-22 21:12:15 +0300

    Dynamically Binding Directives to properties - S7, L98

--- src/app/better-highlight-directive/better-highlight.directive.ts
+++ src/app/better-highlight-directive/better-highlight.directive.ts
@@ -5,22 +5,25 @@ import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Rendere
 })
 export class BetterHighlightDirective implements OnInit{
 
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue;'
   @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 
