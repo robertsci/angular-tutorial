9149ba9920e2e73363bfbff1ade9eba537359e85
Robert Constantinescu <robertsci@github.com>
2020-05-22 18:54:47 +0300

    recap on *ngFor & *ngIf - L7, S7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from "./success-alert/success-alert.component";
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
