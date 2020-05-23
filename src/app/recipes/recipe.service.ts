805aaefd52586366e3abbe957a909572aacb5de2
Robert Constantinescu <robertsci@github.com>
2020-05-23 10:54:35 +0300

    added ingredients to the Recipe -> L121

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -8,10 +9,12 @@ export class RecipeService {
   private _recipes: Recipe[] = [
     new Recipe("CheeseCake",
       "cookie with cheese",
-      "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646"),
-    new Recipe("CheeseCake 2",
+      "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646",
+      [new Ingredient("Cheese", 1), new Ingredient("Visine", 1)] ),
+    new Recipe("CarrotCake 2",
       "cookie with cheeseeeeeeeeeeeeeeee",
-      "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646")
+      "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_1064/k%2Farchive%2F7b084eaf9d7d564dd2667094c3dd1260a5e4d646",
+      [new Ingredient("Carrots", 5), new Ingredient("Blat", 4)] )
 
   ];
 
