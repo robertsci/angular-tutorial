060a669b514e4f145d88d3d909338ce036f6a3b3
Robert Constantinescu <robertsci@github.com>
2020-05-23 10:00:55 +0300

    adding an EventEmitter to be available in the whole application really easy from the AccountService -L111

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,6 +12,9 @@ export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
               private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert("New Status: " + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
