8e5fc9961391af0502ab7c74d9e3f2669d48d525
Robert Constantinescu <robertsci@github.com>
2020-05-23 09:55:23 +0300

    by Adding the LoggingService in the providers of app-module, it means the same instance of the LoggingService will be available app wide. This is also useful to inject a service in another service, but in order to do that the services must be annotated with the @Injectable() - L111

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,8 +1,9 @@
+import {Injectable} from "@angular/core";
+
+
+@Injectable()
 export class LoggingService {
   logStatusChange(status: string) {
     console.log("A server status changed. New status: " + status);
   }
-
-
-
 }
