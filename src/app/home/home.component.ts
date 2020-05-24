3ebb8527ca9718d808e596974e40a8e5d2d3f3de
Robert Constantinescu <robertsci@github.com>
2020-05-24 18:56:59 +0300

    adding a custom observable and created logic to unsubscribe when you leave the component - Section 13

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-
+    this.firstObservableSubscription = interval(1000).subscribe(count => {
+      console.log(count)
+    });
   }
+
+  ngOnDestroy(): void {
+    this.firstObservableSubscription.unsubscribe();
+  }
+
+
+
 
 }
