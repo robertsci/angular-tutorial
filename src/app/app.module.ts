71f37d948330f816a21db1b8aea6f0c7f7adcfab
Robert Constantinescu <robertsci@github.com>
2020-05-24 18:51:36 +0300

    initial project structure - Section 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from "./success-alert/success-alert.component";
-import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    HomeComponent,
+    UserComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
