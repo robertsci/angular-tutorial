8d3c7e9e88462ad45f3cf9a210b8d212d76e52de
Robert Constantinescu <robertsci@github.com>
2020-05-24 16:53:58 +0300

    refactored too add Routing to the app - L155

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -28,8 +29,9 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
     DropdownDirective
   ],
     imports: [
-        BrowserModule,
-        FormsModule
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule
     ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
