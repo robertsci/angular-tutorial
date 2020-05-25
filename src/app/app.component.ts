3247a419ea54b2d28c6972bdb8e6d4224c88737c
Robert Constantinescu <robertsci@github.com>
2020-05-25 18:45:45 +0300

    resetting the form and all of the properties after you click submit the button - L199

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,5 +55,7 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
 
+    this.signupForm.reset();
+
   }
 }
