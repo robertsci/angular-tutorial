ad5648aee6b9120bb4e0ab231468e13bee02058a
Robert Constantinescu <robertsci@github.com>
2020-05-27 20:08:17 +0300

    outsourcing the HttpRequests to a Service - L 260

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from "rxjs/operators";
 import {Post} from "./post.model";
+import {PostsService} from "./posts.service";
 
 @Component({
   selector: 'app-root',
@@ -12,49 +13,24 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http.post<{ name: string }>(
-      'https://angular-first-project-97484.firebaseio.com/posts.json',
-      postData
-    ).subscribe(responseData =>{
-      console.log(responseData);
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
 
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts() {
-    this.isFetching =true;
-    this.http
-      .get<{ [key: string]: Post; }>('https://angular-first-project-97484.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if(responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key });
-          }}
-        return postsArray;
-      }))
-      .subscribe(
-        posts => {
-          this.isFetching = false;
-          this.loadedPosts = posts;
-        }
-    );
-
-  }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Post} from "./post.model";
+import {map} from "rxjs/operators";
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  createAndStorePost(title: string, content: string) {
+    // Send Http request
+    const postData: Post = {title: title, content: content};
+    this.httpClient.post<{ name: string }>(
+      'https://angular-first-project-97484.firebaseio.com/posts.json',
+      postData
+    ).subscribe(responseData =>{
+      console.log(responseData);
+    });
+  }
+
+  fetchPosts() {
+    this.httpClient
+      .get<{ [key: string]: Post; }>('https://angular-first-project-97484.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(
+        posts => {
+        }
+      );
+  }
+}
