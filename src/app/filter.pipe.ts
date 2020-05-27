de1db8d213c2959af39321a7308367049a2192ae
Robert Constantinescu <robertsci@github.com>
2020-05-27 07:39:20 +0300

    creating a custom filter pipe -> L 246

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {filter} from "rxjs/operators";
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string, propertyName: string): unknown {
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propertyName] === filterString ) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+  }
+
+}
