2ef27e1e346b744ea8f34b58909e0bc96b384766
Robert Constantinescu <robertsci@github.com>
2020-05-27 20:46:10 +0300

    Using the catch- throw error strategy - L 265

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from "rxjs/operators";
 import {Post} from "./post.model";
 import {PostsService} from "./posts.service";
 import {Subscription} from "rxjs";
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "./post.model";
-import {map} from "rxjs/operators";
-import {Subject} from "rxjs";
+import {map, catchError} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -37,7 +37,10 @@ export class PostsService {
           }
         }
         return postsArray;
-      })
+      }),
+        catchError(errorResponse => {
+         return throwError(errorResponse);
+        })
       );
   }
 
