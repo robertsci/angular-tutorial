281481edf8c2c124c3c0a23caa5094147e86d087
Robert Constantinescu <robertsci@github.com>
2020-05-28 20:56:41 +0300

    interact with the RESPONSE  with INTERCEPTOR, you can alter it inside the if statement - L 272

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
 
@@ -7,7 +8,15 @@ export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log("request is on it's way");
     const modifiedRequest = req.clone({headers: req.headers.append('auth',' xyz')});
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap (
+      event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log("Response Arrived. Body Data: ");
+          console.log(event.body);
+        }
+      }
+    ));
   }
 
 
