21cbc2fec473f61141cd3a6b1124c7b35cbba2f0
Robert Constantinescu <robertsci@github.com>
2020-05-28 21:32:12 +0300

    Storing recipes into firebase with a PUT request - L 280

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,27 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'}) // used when you inject a service into another service. Here we inject the HttpService
 export class DataStorageService {
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              private recipeService: RecipeService) {
 
+  }
+
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes;
+    console.log("Recipes to Save: ");
+    console.log(recipes);
+    this.httpClient.put(
+      'https://angular-first-project-97484.firebaseio.com/recipes.json',
+      recipes)
+      .subscribe(
+        response => {
+          console.log(response);
+        });
   }
 
 }
