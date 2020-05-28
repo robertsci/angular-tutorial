dc05d70cd31d3134d98834ec845a9e02c40e45d4
Robert Constantinescu <robertsci@github.com>
2020-05-28 21:03:07 +0300

    adding another Logging Interceptor. BE AWARE: the order of the importing them matters. They intercept in the imported order - L 273

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {AuthInterceptorService} from "./auth-interceptor.service";
+import {LoggingInterceptorService} from "./logging-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,8 +18,13 @@ import {AuthInterceptorService} from "./auth-interceptor.service";
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
       multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptorService,
+      multi: true
     }
-    ],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,22 +1,12 @@
-import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
 
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("request is on it's way");
     const modifiedRequest = req.clone({headers: req.headers.append('auth',' xyz')});
-    return next.handle(modifiedRequest).pipe(tap (
-      event => {
-        console.log(event);
-        if (event.type === HttpEventType.Response) {
-          console.log("Response Arrived. Body Data: ");
-          console.log(event.body);
-        }
-      }
-    ));
+    return next.handle(modifiedRequest);
   }
 
 
