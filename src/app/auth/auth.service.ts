681670bc8bdac3a15f62cc200b1f1a8be823b8ba
Robert Constantinescu <robertsci@github.com>
2020-07-04 17:31:12 +0300

    L297 - added some error handling for the sign in process

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
@@ -29,18 +29,11 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }).pipe(
-        catchError (errorResponse => {
-          let errorMessage = 'An unknown error occurred';
-          if (!errorResponse.error || !errorResponse.error.error) {
-            return throwError(errorMessage)
-          }
-          switch (errorResponse.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists';
-          }
-          return throwError(errorMessage);
-        })
+      catchError(
+        this.handleError
+      )
     );
+
   }
 
   login(email: string, password: string) {
@@ -49,6 +42,33 @@ export class AuthService {
         email: email,
         password: password,
         returnSecureToken: true
-      }).pipe();
+      }).pipe(
+      catchError(
+        this.handleError
+      )
+    );
   }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+      let errorMessage = 'An unknown error occurred';
+      if (!errorResponse.error || !errorResponse.error.error) {
+        return throwError(errorMessage)
+      }
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email already exists';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errorMessage = 'This email does not exists';
+          break;
+        case 'INVALID_PASSWORD':
+          errorMessage = 'Incorrect Password';
+          break;
+      }
+      return throwError(errorMessage);
+    }
+
+
 }
+
+
