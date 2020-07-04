124aea29921ce5cead6f3d858a664402fce65700
Robert Constantinescu <robertsci@github.com>
2020-07-04 11:28:57 +0300

    L295 - Improving Error handling logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -40,9 +40,9 @@ export class AuthComponent {
           console.log(responseData);
           this.isLoading = false;
         },
-        error => {
-          console.log(error);
-          this.error = 'An error occurred!';
+        errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage;
           this.isLoading = false;
         }
       );
