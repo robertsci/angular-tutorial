dc8f1a72d4843f0bde69ce1365bbee4e53378ddf
Robert Constantinescu <robertsci@github.com>
2020-07-04 20:18:49 +0300

    L303 - added AutoLogin functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -53,10 +53,29 @@ export class AuthService {
     this.router.navigate(['/auth'])
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000)) //creating a new date where we add seconds received from the firebase
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
