025eb79e95befde53b9decaeae352906c2464914
Robert Constantinescu <robertsci@github.com>
2020-07-04 18:26:30 +0300

    L299 - Reflecting the Authentication logic in the webpage

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -17,7 +18,7 @@ export class AuthComponent {
   error: string = null;
 
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   onSwitchMode() {
@@ -45,6 +46,7 @@ export class AuthComponent {
       responseData => {
         console.log(responseData);
         this.isLoading = false;
+        this.router.navigate(['/recipes'])
       },
       errorMessage => {
         console.log(errorMessage);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,37 @@
-import {Component} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false
+  private userSubscription: Subscription;
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe(
+      user => {
+        this.isAuthenticated = !!user; //some angular feature, search the web for explanation
+        console.log(!user);
+        console.log(!!user);
+      }
+    );
   }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+
+
+
+
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
