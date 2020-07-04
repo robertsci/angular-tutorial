3ace10353f8ef0546e0ecb66f8744ca154820f06
Robert Constantinescu <robertsci@github.com>
2020-07-04 20:06:54 +0300

    L302 - Adding logout logic

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError, tap} from "rxjs/operators";
 import {BehaviorSubject, Subject, throwError} from "rxjs";
 import {User} from "./user.model";
+import {Router} from "@angular/router";
 
 export interface AuthResponseData {
   idToken: string;
@@ -21,7 +22,7 @@ export class AuthService {
   user = new BehaviorSubject<User>(null);
 
 
-  constructor(private http: HttpClient ) {
+  constructor(private http: HttpClient, private router: Router ) {
   }
 
   signup(email: string, password: string) {
@@ -45,6 +46,11 @@ export class AuthService {
       }).pipe(catchError(this.handleError), tap(responseData => {
       this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn)
     }));
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth'])
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
