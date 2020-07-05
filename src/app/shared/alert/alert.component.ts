1b21d60d89982c83accd77df4a197c619abce679
Robert Constantinescu <robertsci@github.com>
2020-07-05 12:14:07 +0300

    L311 - added functionality to close the modal component

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
   selector: 'app-alert',
@@ -7,5 +7,11 @@ import {Component, Input} from "@angular/core";
 })
 export class AlertComponent {
   @Input() message: any;
+  @Output() close = new EventEmitter<void>();
+
+
+  onClose() {
+    this.close.emit();
+  }
 
 }
